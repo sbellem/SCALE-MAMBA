c6894cd991451f3529865822f5d83c38243c7b7e
Sylvain Bellemare <sbellem@gmail.com>
2019-12-10 02:38:42 -0500

    a

--- .ci/run_tests.sh
+++ .ci/run_tests.sh
@@ -2,6 +2,15 @@
 
 set -ev
 
+cp -R Auto-Test-Data/Cert-Store/* Cert-Store/
+
+for i in $(seq 0 25); do
+  \rm -f Scripts/logs/$i
+done
+
+cp -R Auto-Test-Data/$TEST_SET/* Data/
+echo Running testscript on set $TEST_SET
+
 programs=(
     test_array
     test_branch
@@ -26,14 +35,5 @@ programs=(
     #test_fix_array
     #test_all
 )
-
-cp -R Auto-Test-Data/Cert-Store/* Cert-Store/
-
-for i in $(seq 0 25); do
-  \rm -f Scripts/logs/$i
-done
-
-cp -R Auto-Test-Data/$TEST_SET/* Data/
-echo Running testscript on set $TEST_SET
 
 ./Scripts/test-batch.sh "${programs[@]}"
