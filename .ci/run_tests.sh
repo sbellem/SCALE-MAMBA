0b812005599f848c208fdf2e062676f5decb4ae4
Sylvain Bellemare <sbellem@gmail.com>
2019-12-10 00:24:52 -0500

    a

--- .ci/run_tests.sh
+++ .ci/run_tests.sh
@@ -2,10 +2,30 @@
 
 set -ev
 
-programs=(test_array test_branch test_branching test_comparison test_empty_tape
-          test_flex test_float test_floatingpoint test_float_sorting test_float_vector
-          test_function test_idle_threads test_lib test_loop test_mem_order test_sregint
-          test_vector test_sfix test_sqrt test_custom_array test_fix_array test_all)
+programs=(
+    test_array
+    test_branch
+    test_branching
+    test_comparison
+    test_empty_tape
+    test_flex
+    test_float
+    test_floatingpoint
+    test_float_sorting
+    test_float_vector
+    test_function
+    test_idle_threads
+    test_lib
+    test_loop
+    test_mem_order
+    test_sregint
+    test_vector
+    test_sfix
+    test_sqrt
+    test_custom_array
+    test_fix_array
+    test_all
+)
 
 cp -R Auto-Test-Data/Cert-Store/* Cert-Store/
 
